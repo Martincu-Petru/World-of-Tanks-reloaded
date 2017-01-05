f5ea17ff5e601e58012f9a2e239d578be56f26fd
Marțincu Petru <martincu.petru@gmail.com>
2017-01-05 14:51:54 +0200

    Added pickups
    Not yet interracting with the player, only spawning randomly

--- World of Tanks reloaded/World of Tanks reloaded/main.cpp
+++ World of Tanks reloaded/World of Tanks reloaded/main.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include "bullet.h"
 #include <sstream> 
-#include "Pickup.h"
 #include "ai_Entity.h"
 #include "AI_Chaser.h"
 #include <stdlib.h>
@@ -15,7 +14,6 @@ using namespace std;
 int main()
 {
 	RenderWindow mainScreen(VideoMode(1366, 768), "World of Tanks reloaded", Style::Fullscreen);
-	mainScreen.setKeyRepeatEnabled(true);
 	Player mainTank("E-100.png");
 
 	ai_Entity mainAI("Ferdinand.png");
@@ -25,19 +23,24 @@ int main()
 	Bullet bullets[2];
 	int currentBullet = 0, i;
 	
-	//Pickup ammoPickup(2);
-
 	Event Event;
 	Clock tictoc, elapsed;;
-	Texture healthBarTexture, textureBackground, stone, planeTexture, plane2Texture;
-	Sprite HP, spriteBackground, stone0, plane, plane2;
+	Texture healthBarTexture, textureBackground, stone, planeTexture, plane2Texture, healthTexture, speedTexture, ammoTexture;
+	Sprite HP, spriteBackground, stone0, plane, plane2, healthSprite, ammoSprite, speedSprite;
 	RectangleShape healthBar, healthBarEmpty;
 	Text healthLevel;
 	Font fontHealth;
-	bool planeActive = false, plane2Active = false;
+	bool planeActive = false, plane2Active = false, ammoSpawned=false, speedSpawned=false, healthSpawned=false;
 	float planeSpeed = 0.0f, plane2Speed = 0.0f;
 	int health = mainTank.getHealth();
+	Clock clock, ammoClock, speedClock, healthClock, timeGone, ammoDespawnClock, speedDespawnClock, healthDespawnClock;
 
+
+	mainScreen.setKeyRepeatEnabled(true);
+
+	healthTexture.loadFromFile("health.png");
+	speedTexture.loadFromFile("speed.png");
+	ammoTexture.loadFromFile("ammo.png");
 	healthBarTexture.loadFromFile("HP.png");
 	planeTexture.loadFromFile("Ao192.png");
 	textureBackground.loadFromFile("intro.jpg");
@@ -45,7 +48,9 @@ int main()
 	plane2Texture.loadFromFile("Bf110e.png");
 	fontHealth.loadFromFile("Font.ttf");
 
-
+	healthSprite.setTexture(healthTexture);
+	ammoSprite.setTexture(ammoTexture);
+	speedSprite.setTexture(speedTexture);
 	HP.setTexture(healthBarTexture);
 	spriteBackground.setTexture(textureBackground);
 	stone0.setTexture(stone);
@@ -60,6 +65,10 @@ int main()
 	plane2.scale(0.3f, 0.3f);
 	healthBarEmpty.setPosition(1070, 30);
 
+
+	healthSprite.setScale(0.2f, 0.2f);
+	ammoSprite.setScale(0.1f, 0.1f);
+	speedSprite.setScale(0.15f, 0.15f);
 
 	HP.setScale(0.4f, 0.4f); 
 	plane.scale(0.3f, 0.3f);
@@ -76,17 +85,12 @@ int main()
 	healthLevel.setCharacterSize(30);
 	healthLevel.setFillColor(Color::White);
 
-	Pickup healthPickup(1);
-
-	healthPickup.spawn();
-	//ammoPickup.spawn();
 
 	while (mainScreen.isOpen()){
-
 		healthLevel.setPosition(1017, 65);
-		Time dt = tictoc.restart(); //Timpul dintre 2 treceri
-
-
+		Time dt = clock.restart(), speedSinceSpawn=speedClock.getElapsedTime(),
+			healthSinceSpawn= healthClock.getElapsedTime(), ammoSinceSpawn = ammoClock.getElapsedTime();
+		Time ammoSinceDespawn=ammoDespawnClock.getElapsedTime(), healthSinceDespawn=healthDespawnClock.getElapsedTime(), speedSinceDespawn=speedDespawnClock.getElapsedTime();
 		if (!planeActive)
 		{
 			srand((int)time(0));
@@ -94,7 +98,7 @@ int main()
 			srand((int)time(0) * 10);
 			float height = rand() % 500 + 100;
 			plane.setPosition(1400, height);
-			if (rand() % 10 == 9) //Sansele sunt 1 din 7 ca avionul sa fie spawn-at din nou
+			if (rand() % 10 == 9)
 				planeActive = true;
 		}
 		else
@@ -111,7 +115,7 @@ int main()
 			srand((int)time(0) * 10);
 			float height2 = rand() % 500 + 100;
 			plane2.setPosition(-1400, height2);
-			if (rand() % 7 == 0) //Sansele sunt 1 din 7 ca avionul sa fie spawn-at din nou
+			if (rand() % 7 == 0)
 				plane2Active = true;
 		}
 		else
@@ -119,6 +123,59 @@ int main()
 			plane2.setPosition(plane2.getPosition().x + (plane2Speed * dt.asSeconds()), plane2.getPosition().y);
 			if (plane2.getPosition().x > 1400)
 				plane2Active = false;
+		}
+		if (!ammoSpawned && ammoSinceDespawn.asSeconds()>2)
+		{
+			srand(time(0)*ammoSinceDespawn.asSeconds());
+			int x = rand() % 1200 + 100;
+			srand(time(0)*ammoSinceDespawn.asSeconds()+3.14);
+			int y = rand() % 500 + 10;
+			ammoSprite.setPosition(x, y);
+			if (healthSpawned == true && abs(x - healthSprite.getPosition().x) < 100 || abs(x - speedSprite.getPosition().x) < 100 && speedSpawned == true)
+			{
+				cout << "Nup";
+			}
+			else
+			{
+				ammoSpawned = true;
+				ammoClock.restart();
+			}
+		}
+		if (!healthSpawned && healthSinceDespawn.asSeconds()>3)
+		{
+			srand(time(0)*healthSinceDespawn.asSeconds());
+			int x = rand() % 1200 + 100;
+			srand(time(0)*healthSinceDespawn.asSeconds()+3.14);
+			int y = rand() % 500 + 10;
+			healthSprite.setPosition(x, y);
+			if (ammoSpawned == true && abs(x - ammoSprite.getPosition().x) < 100 || abs(x - speedSprite.getPosition().x) < 100 && speedSpawned == true)
+			{
+				cout << "Nup";
+			}
+			else
+			{
+				healthSpawned = true;
+				healthClock.restart();
+			}
+
+
+		}
+		if (!speedSpawned && speedSinceDespawn.asSeconds()>4)
+		{
+			srand(time(0)*speedSinceDespawn.asSeconds());
+			int x = rand() % 1200 + 100;
+			srand(time(0)*speedSinceDespawn.asSeconds()+3.14);
+			int y = rand() % 500 + 10;
+			speedSprite.setPosition(x, y);
+			if (ammoSpawned == true && abs(x - ammoSprite.getPosition().x) < 100 || abs(x - healthSprite.getPosition().x) < 100 && healthSpawned == true)
+			{
+				cout << "Nup";
+			}
+			else
+			{
+				speedSpawned = true;
+				speedClock.restart();
+			}
 		}
 
 			healthBar.setSize(Vector2f(mainTank.getHealth()*2.6, 30));
@@ -128,7 +185,7 @@ int main()
 			//first we have to handle the player's input
 			if (Keyboard::isKeyPressed(Keyboard::S) || Keyboard::isKeyPressed(Keyboard::Down))
 			{
-				cout << endl << " DA " << endl;
+			//	cout << endl << " DA " << endl;
 				mainTank.movePlayer('u', 0.75); //era 1.5 peste tot dar tancul se misca prea repede
 			}
 			else if (Keyboard::isKeyPressed(Keyboard::D) || Keyboard::isKeyPressed(Keyboard::Right))
@@ -197,10 +254,10 @@ int main()
 
 
 			float rot = mainTank.rotationOfPlayer();
-			cout << " coordonate xA yA " << xA << " " << yA << endl;
-			cout << "raza cercului : " << radius << endl;
-			cout << "lungimea laturii patratului : " << L << endl;
-			cout << "rotatie : " << chaserRotation << " " << rot << endl << endl;
+			//cout << " coordonate xA yA " << xA << " " << yA << endl;
+			//cout << "raza cercului : " << radius << endl;
+			//cout << "lungimea laturii patratului : " << L << endl;
+			//cout << "rotatie : " << chaserRotation << " " << rot << endl << endl;
 			//to here for chaser
 
 
@@ -213,33 +270,42 @@ int main()
 				}
 			}
 
-
-
 		mainScreen.clear();
 		mainScreen.draw(spriteBackground);
 		stringstream ss;
 		ss << "Integrity: " << mainTank.getHealth();
 		healthLevel.setString(ss.str());
+
 		mainTank.drawPlayer(mainScreen);
-		/*if (healthPickup.isSpawned())
-		{
-			mainScreen.draw(healthPickup.getSprite());
-		}
-		if (ammoPickup.isSpawned())
-		{
-			mainScreen.draw(ammoPickup.getSprite());
-		}*/
-
-		mainScreen.draw(healthPickup.getSprite());
-		//mainScreen.draw(ammoPickup.getSprite());
-
-		//mainScreen.draw(stone0);
+		mainScreen.draw(stone0);
 		mainScreen.draw(plane);
 		mainScreen.draw(plane2);
 		mainScreen.draw(healthLevel);
 		mainScreen.draw(healthBarEmpty);
 		mainScreen.draw(healthBar);
 		mainScreen.draw(HP);
+		if (ammoSinceSpawn.asSeconds() > 3)
+		{
+			ammoClock.restart();
+			ammoSpawned = false;
+		}
+		if (healthSinceSpawn.asSeconds() > 4)
+		{
+			healthClock.restart();
+			healthSpawned = false;
+		}
+		if (speedSinceSpawn.asSeconds() > 5)
+		{
+			speedClock.restart();
+			speedSpawned = false;
+		}
+		if(ammoSpawned)
+			mainScreen.draw(ammoSprite);
+		if (healthSpawned)
+			mainScreen.draw(healthSprite);
+		if (speedSpawned)
+			mainScreen.draw(speedSprite);
+
 		for (i = 0; i < 2; ++i)
 		{
 			if (bullets[i].isInFlight())
